a990b89aab798f882f100c66ccde3ab6cbf266e2
AbdulkodirBurganov <a.burganov@fil-it.ru>
2021-12-24 16:01:53 +0300

    returned album

--- Library.py
+++ Library.py
@@ -1,5 +1,6 @@
 class Library:
-    def __init__(self, name, author, year):
+    def __init__(self, name, author, year, album):
         self.name = name
         self.author = author
         self.year = year
+        self.album = album
